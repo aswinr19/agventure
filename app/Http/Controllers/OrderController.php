994a3ba6ff2e594d86b5b9a6e14e131bbfe2575e
Aswin r <aswinr019@gmail.com>
2022-04-21 20:20:17 +0530

    added orderController actions

--- app/Http/Controllers/OrderController.php
+++ app/Http/Controllers/OrderController.php
@@ -2,33 +2,83 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Purchase;
 use Illuminate\Http\Request;
 
 class OrderController extends Controller
 {
-    public function index(){
+    public function index(Request $request){
+
+        $orders = Purchase::latest()->get();
+
+        return view('orders.index',['title'=>'Orders page','orders'=>$orders]);        
 
     }
     public function show($id){
 
+        $order = Purchase::findOrFail($id);
+
+        return view('orders.show',['title' => 'Order page','order'=> $order]);
+
     }
     public function packed($id){
+
+        $order = Purchase::findOrFail($id);
+
+        $order->orderStatus = "packed";
+
+        $order->save();
+
+        return redirect('/admin/orders/{$id}');
 
     }
     public function shipped($id){
 
+        $order = Purchase::findOrFail($id);
+
+        $order->orderStatus = "shipped";
+
+        $order->save();
+
+        return redirect('/admin/orders/{$id}');
+
     }
     public function delivered($id){
 
+        $order = Purchase::findOrFail($id);
+
+        $order->orderStatus = "delivered";
+
+        $order->save();
+
+        return redirect('/admin/orders/{$id}');
     }
-    public function display(){
+    public function display(Request $request){
+        
+        $id = $request->session()->get('loggedUser');    
+        
+        $orders = Purchase::latest()
+            ->where('user_id',$id)
+                ->get();
+
+                return view('orders.display',['title' => 'Orders page','orders'=> $orders]);
 
     }
     public function displayOne($id){
+ 
+        $order = Purchase::findOrFail($id);
 
+        return view('orders.displayOne',['title' => 'Order page','order'=> $order]);
     }
     public function cancel($id){
 
+        $order = Purchase::findOrFail($id);
+
+        $order->orderStatus = "cancelled";
+
+        $order->save();
+
+        return redirect('/orders/{$id}');
     }
 
 }
---
+++ resources/views/orders/index.blade.php
@@ -0,0 +1,4 @@
+@extends('layouts.layout')
+@section('content')
+
+@endsection
