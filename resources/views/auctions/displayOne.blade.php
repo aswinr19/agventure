7be3d7a5f3046787601566a02a994397c8e11329
Aswin r <aswinr019@gmail.com>
2022-04-30 21:32:41 +0530

    added task scheduler for checking auction status, added logic for deciding auction winner

--- resources/views/auctions/displayOne.blade.php
+++ resources/views/auctions/displayOne.blade.php
@@ -21,11 +21,19 @@
 {{
 $auction->status}}<br>
 <img src="{{asset('images/'. $auction->item->image)}}" alt="{{ $auction->item->image }} " height="40px">
+Current Highest Bid : <br>
 <form action="" method="POST">
-    <label for="amount">Amount</label>
+    <label for="amount">Bid Amount</label>
     <input type="text" name="amount"><br>
     <input type="checkbox" name="agree" > Agree to terms and conditions
     <input type="submit" name="submit" value="Enter Auction">
 </form>
+
+<form action="" method="POST">
+    <label for="amount">New Bid Amount</label>
+    <input type="text" name="amount"><br>
+    <input type="submit" name="submit" value="Update Bid Amount">
+</form>
+
 <a href="">Quit auction</a>
 @endsection
