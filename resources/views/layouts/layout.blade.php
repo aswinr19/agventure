4af1afd023690abece4da0d03e72673dca6c7851
Aswin r <aswinr019@gmail.com>
2022-05-01 19:46:51 +0530

    changed the font to quicksand

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -7,12 +7,14 @@
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('plant(1).png') }}">
         <link rel="preconnect" href="https://fonts.googleapis.com">
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet"> 
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&family=Quicksand:wght@300&display=swap" rel="stylesheet"> 
         <title>{{$title}}</title>
         <style>
         body {
+
             font-family: 'Montserrat', sans-serif;
-    
+            font-family: 'Quicksand', sans-serif;
+            
             }
             nav,ul{
                 display: flex;
