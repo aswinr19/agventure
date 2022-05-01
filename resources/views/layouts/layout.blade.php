a6db03949bb21b8f668c5f14a5ddc0d676be26f4
lokiOdin1 <104724181+lokiOdin1@users.noreply.github.com>
2022-05-01 19:30:09 +0530

    Update layout.blade.php
    Changed the font to a more suitable one called montserrat.

--- resources/views/layouts/layout.blade.php
+++ resources/views/layouts/layout.blade.php
@@ -5,10 +5,13 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <link rel="shortcut icon" type="image/x-icon" href="{{ asset('plant(1).png') }}">
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@100&display=swap" rel="stylesheet"> 
         <title>{{$title}}</title>
         <style>
         body {
-            font-family: "sans-serif";
+            font-family: 'Montserrat', sans-serif;
     
             }
             nav,ul{
